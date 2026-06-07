837fbd8d4c5277ae3d409079fe47919f16ed0377
DESKTOP-J0ABEDB\이명재 <love12mj@naver.com>
2026-06-07 15:15:32 +0900

    init

---
+++ src/main/java/com/lee/android_device_control/AndroidDeviceControlApplication.java
@@ -0,0 +1,13 @@
+package com.lee.android_device_control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndroidDeviceControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AndroidDeviceControlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lee/android_device_control/AndroidDeviceControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lee.android_device_control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndroidDeviceControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
